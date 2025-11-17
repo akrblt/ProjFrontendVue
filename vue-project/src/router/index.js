5b8104367daaad1b3176b190a5e1e5dbab27f412
Ahmet Karabulut <a.karabulut070@gmail.com>
2025-11-17 10:38:11 +0100

    feat : serach bar

---
+++ vue-project/src/router/index.js
@@ -0,0 +1,16 @@
+
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../pages/Home.vue';
+import Detail from '../pages/Detail.vue';
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/movie/:id', component: Detail }
+];
+
+const router= createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
