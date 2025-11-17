de100de6c8c1d45c2d63561a5bc079345c62b3d9
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2025-11-17 14:24:41 +0100

    feat:erreur film non trouvé

--- movie-catalog-app/src/store/movieStore.js
+++ movie-catalog-app/src/store/movieStore.js
@@ -1,4 +1,3 @@
-
 import { defineStore } from 'pinia';
 import { searchMovies, getMovieDetail } from '../services/omdbApi';
 
@@ -12,21 +11,39 @@ export const useMovieStore = defineStore('movie', {
   actions: {
     async fetchMovies(query) {
       this.loading = true;
+      this.error = null;
+      this.movies = [];
       try {
-        const data = await searchMovies(query);
-        this.movies = data.Search || [];
+        const result = await searchMovies(query);
+
+        if (!result.success || result.data.length === 0) {
+          this.error = "Le film n'a pas été trouvé";
+          return;
+        }
+
+        this.movies = result.data;
       } catch (err) {
-        this.error = err.message;
+        this.error = "Le film n'a pas été trouvé";
       } finally {
         this.loading = false;
       }
     },
+
     async fetchMovieDetail(id) {
       this.loading = true;
+      this.error = null;
+      this.selectedMovie = null;
       try {
-        this.selectedMovie = await getMovieDetail(id);
+        const result = await getMovieDetail(id);
+
+        if (!result.success || !result.data) {
+          this.error = "Le film n'a pas été trouvé";
+          return;
+        }
+
+        this.selectedMovie = result.data;
       } catch (err) {
-        this.error = err.message;
+        this.error = "Le film n'a pas été trouvé";
       } finally {
         this.loading = false;
       }
