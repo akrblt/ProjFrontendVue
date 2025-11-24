f22d1a1dc6e341edca30ab4b243168d5644ddf2a
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2025-11-24 14:47:45 +0100

    Revert "Add error gestion omdbApi/searchMovie"
    This reverts commit a7aa7d71ca2dabaccb3a93242ed55f14060539de.

---
+++ vue-project/src/pages/Home.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <div>
+    <SearchBar />
+    <div v-if="movieStore.loading">loading...</div>
+    <div v-if="movieStore.error">{{ movieStore.error }}</div>
+    <div class="movie-list">
+      <MovieCard v-for="movie in movieStore.movies" :key="movie.imdbID" :movie="movie" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import SearchBar from '../components/SearchBar.vue';
+import MovieCard from '../components/MovieCard.vue';
+import { useMovieStore } from '../store/movieStore';
+
+const movieStore = useMovieStore();
+</script>
+
+<style scoped>
+.movie-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+}
+</style>
+
