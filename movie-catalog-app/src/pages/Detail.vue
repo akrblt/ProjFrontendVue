0420eb8b80cfbb833115dd44142ae34b026c17b7
Ahmet Karabulut <a.karabulut070@gmail.com>
2025-11-24 16:22:59 +0100

    feat : merge MovieDetails in Detail.vue

--- movie-catalog-app/src/pages/Detail.vue
+++ movie-catalog-app/src/pages/Detail.vue
@@ -1,25 +1,7 @@
-
-
 <template>
-  <div v-if="movieStore.loading">loading...</div>
-  <div v-else-if="movieStore.selectedMovie">
-    <h1>{{ movieStore.selectedMovie.Title }}</h1>
-    <img :src="movieStore.selectedMovie.Poster" alt="Poster" />
-    <p><strong>Year:</strong> {{ movieStore.selectedMovie.Year }}</p>
-    <p><strong>Genre:</strong> {{ movieStore.selectedMovie.Genre }}</p>
-    <p><strong>Decsription:</strong> {{ movieStore.selectedMovie.Plot }}</p>
-  </div>
+  <MovieDetail />
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
-import { useRoute } from 'vue-router';
-import { useMovieStore } from '../store/movieStore';
-
-const route = useRoute();
-const movieStore = useMovieStore();
-
-onMounted(() => {
-  movieStore.fetchMovieDetail(route.params.id);
-});
+import MovieDetail from '../components/MovieDetail.vue';
 </script>
