ffaf23a1333ef8ea2a855e0e6307cb89772c27c9
Ahmet Karabulut <a.karabulut070@gmail.com>
2025-12-01 14:44:45 +0100

    feat : add navbar with router film category action romance ...

--- movie-catalog-app/src/App.vue
+++ movie-catalog-app/src/App.vue
@@ -1,6 +1,16 @@
 
 <template>
-  <router-view />
+  <div>
+    <nav class="menu">
+      <router-link to="/">Home</router-link>
+      <router-link to="/category/action"></router-link>
+      <router-link to="/category/science">Science</router-link>
+      <router-link to="/category/romance">Romance</router-link>
+
+    </nav>
+
+    <router-view />
+  </div>
 </template>
 
 <script setup>
@@ -12,5 +22,18 @@ body {
   font-family: Arial, sans-serif;
   margin: 0;
   padding: 20px;
+}
+.menu {
+  display: flex;
+  gap: 20px;
+  margin-bottom: 20px;
+}
+.menu a {
+  text-decoration: none;
+  font-weight: bold;
+  color: black;
+}
+.menu a.router-link-active{
+  color: red;
 }
 </style>
